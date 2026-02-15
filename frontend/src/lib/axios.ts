77eb04d79ea166ccfe4319a8c7c9d3535710984f
Ayushirai879-java <raiayushi381@gmail.com>
2026-02-15 18:57:58 +0530

    changes added

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
 	baseURL: "http://localhost:5000/api" ,
+	withCredentials: true,
 });
 
 //import.meta.env.MODE === "development" ? : "/api"
